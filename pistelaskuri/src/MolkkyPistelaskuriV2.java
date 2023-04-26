0f8b1f2516323c197d6b544bf178c6b70d3438f1
jounikivi <jounikivipera@gmail.com>
2023-04-26 08:20:01 +0300

    päivityksiä

--- pistelaskuri/src/MolkkyPistelaskuriV2.java
+++ pistelaskuri/src/MolkkyPistelaskuriV2.java
@@ -3,40 +3,26 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.HashMap;
 
-/**
- * MolkkyPistelaskuri on Java-ohjelma, joka toimii pistelaskurina mölkky-pelissä.
- * Ohjelma kysyy pelaajien lukumäärän ja nimet käyttäjältä, ja näyttää käyttöliittymässä
- * nykyisen pelaajan nimen ja pistetilanteet taulukossa. Käyttäjä voi lisätä pisteitä
- * nykyiselle pelaajalle syöttämällä halutun pistemäärän tekstikenttään ja painamalla
- * "Lisää pisteet" -painiketta. Ohjelma tarkistaa, että syöte on kelvollinen kokonaisluku,
- * lisää pisteet pelaajalle ja päivittää pistetilanteet taulukkoon. Jos pelaaja saa yli
- * 50 pistettä, hän tippuu takaisin 25 pisteeseen. Jos jokin pelaaja saa 50 pistettä,
- * peli päättyy ja ilmoitetaan voittaja.
- */
 
-public class MolkkyPistelaskuri extends JFrame {
-    // Pelaajat tallennetaan HashMap-tietorakenteeseen, jossa avaimena on pelaajan nimi ja
-    // arvona pelaajan pisteet.
+public class MolkkyPistelaskuriV2 extends JFrame {
+    
     private HashMap<String, Integer> pelaajat = new HashMap<String, Integer>();
     private String[] pelaajaNimet;
     private int nykyinenPelaajaIndex = 0;
 
-    // Käyttöliittymäkomponentit
+    
     private JLabel pelaajaLabel;
     private JLabel pisteetLabel;
     private JTextField pisteetTextField;
     private JButton lisaaPisteetButton;
     private JTable taulukko;
 
-     /**
-     * Luo uuden MolkkyPistelaskuri-ikkunan. Kysyy pelaajien lukumäärän ja nimet käyttäjältä,
-     * luo käyttöliittymäkomponentit ja asettaa ne ikkunaan.
-     */
+     
 
-    public MolkkyPistelaskuri() {
+    public MolkkyPistelaskuriV2() {
         super("Mölkky Pistelaskuri");
 
-        // Kysy pelaajien lukumäärä ja nimet
+        
         int pelaajienLkm = Integer.parseInt(JOptionPane.showInputDialog("Anna pelaajien lukumäärä:"));
         for (int i = 1; i <= pelaajienLkm; i++) {
             String nimi = JOptionPane.showInputDialog("Anna pelaajan " + i + " nimi:");
@@ -44,7 +30,7 @@ public class MolkkyPistelaskuri extends JFrame {
         }
         pelaajaNimet = pelaajat.keySet().toArray(new String[0]);
 
-        // Luodaan käyttöliittymäkomponentit
+       
         pelaajaLabel = new JLabel(pelaajaNimet[0]);
         pisteetLabel = new JLabel("Kuinka monta pistettä lisätään? (1-12):");
         pisteetTextField = new JTextField();
@@ -55,7 +41,7 @@ public class MolkkyPistelaskuri extends JFrame {
             }
         });
 
-        // Luodaan taulukko pelaajien pistetilanteille
+       
         String[] otsikot = {"Pelaaja", "Pisteet"};
         Object[][] data = new Object[pelaajat.size()][2];
         int i = 0;
@@ -66,10 +52,10 @@ public class MolkkyPistelaskuri extends JFrame {
         }
         taulukko = new JTable(data, otsikot);
 
-        // Muokataan pisteetTextFieldin kokoa
+   
         pisteetTextField.setPreferredSize(new Dimension(20, 20));
 
-        // Asetetaan käyttöliittymäkomponentit ikkunaan
+       
           
                 setLayout(new BorderLayout());
                 JPanel ylaPaneeli = new JPanel(new FlowLayout());
@@ -81,24 +67,22 @@ public class MolkkyPistelaskuri extends JFrame {
                 JScrollPane taulukkoPaneeli = new JScrollPane(taulukko);
                 add(taulukkoPaneeli, BorderLayout.CENTER);
         
-                // Asetetaan ikkunan koko ja näkyvyys
+               
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                setSize(1600, 1000);
+                setSize(400, 300);
                 setLocationRelativeTo(null);
                 setVisible(true);
             }
 
-            /**
-             * Metodi lisää pelaajalle annetun pistemäärän ja päivittää taulukon näytön.
-             */
+        
             private void lisaaPisteet() {
                 try {
                     int pisteet = Integer.parseInt(pisteetTextField.getText());
             
-                    // Tarkista että pisteet ovat sallitulla alueella 0-12
+                    
                     if (pisteet < 0 || pisteet > 12) {
                         JOptionPane.showMessageDialog(this, "Syötä luku väliltä 0-12.");
-                        return; // Palaa metodiin ja älä lisää pisteitä
+                        return; 
                     }
             
                     String nykyinenPelaaja = pelaajaNimet[nykyinenPelaajaIndex];
@@ -132,5 +116,3 @@ public class MolkkyPistelaskuri extends JFrame {
                 new MolkkyPistelaskuri();
             }
         }
-        
-       
---
+++ pistelaskuri/src/uusiversio/PeliOhjelma.java
@@ -0,0 +1,123 @@
+package uusiversio;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PeliOhjelma extends JFrame implements ActionListener {
+
+    private JLabel pelaajaNimi;
+    private JTextField pistemaaraKentta;
+    private JTable pistetaulukko;
+    private DefaultTableModel taulukkoMalli;
+
+    private String[] pelaajat;
+    private int[] pisteet;
+
+    private int nykyinenPelaaja;
+    private boolean peliPaattynyt;
+
+    public PeliOhjelma() {
+        super("Peli");
+
+        pelaajienLukumaaraJaNimet();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLayout(new BorderLayout());
+
+        pelaajaNimi = new JLabel("");
+        pelaajaNimi.setHorizontalAlignment(JLabel.CENTER);
+        add(pelaajaNimi, BorderLayout.NORTH);
+
+        taulukkoMalli = new DefaultTableModel();
+        taulukkoMalli.addColumn("Pelaaja");
+        taulukkoMalli.addColumn("Pisteet");
+
+        pistetaulukko = new JTable(taulukkoMalli);
+        add(new JScrollPane(pistetaulukko), BorderLayout.CENTER);
+
+        JPanel alapaneeli = new JPanel(new FlowLayout());
+
+        pistemaaraKentta = new JTextField(10);
+        alapaneeli.add(pistemaaraKentta);
+
+        JButton lisaaPisteetNappi = new JButton("Lisää pisteet");
+        lisaaPisteetNappi.addActionListener(this);
+        alapaneeli.add(lisaaPisteetNappi);
+
+        add(alapaneeli, BorderLayout.SOUTH);
+
+        nykyinenPelaaja = 0;
+        peliPaattynyt = false;
+
+        pelaajanTiedot();
+
+        setVisible(true);
+    }
+
+    private void pelaajienLukumaaraJaNimet() {
+        int lukumaara = Integer.parseInt(JOptionPane.showInputDialog(this, "Syötä pelaajien lukumäärä:"));
+        pelaajat = new String[lukumaara];
+        pisteet = new int[lukumaara];
+        for (int i = 0; i < lukumaara; i++) {
+            pelaajat[i] = JOptionPane.showInputDialog(this, "Syötä pelaajan " + (i+1) + " nimi:");
+        }
+    }
+
+    private void pelaajanTiedot() {
+        pelaajaNimi.setText(pelaajat[nykyinenPelaaja]);
+        taulukkoMalli.setRowCount(0);
+        for (int i = 0; i < pelaajat.length; i++) {
+            taulukkoMalli.addRow(new Object[]{pelaajat[i], pisteet[i]});
+        }
+    }
+
+  
+    private void lisaaPisteet() {
+      if (peliPaattynyt) {
+          return;
+      }
+      String syote = pistemaaraKentta.getText();
+      int lisattavatPisteet;
+      try {
+          lisattavatPisteet = Integer.parseInt(syote);
+          if (lisattavatPisteet < 0 || lisattavatPisteet > 12) {
+              JOptionPane.showMessageDialog(this, "Syötä kelvollinen pistemäärä väliltä 0-12.");
+              return;
+          }
+      } catch (NumberFormatException e) {
+          JOptionPane.showMessageDialog(this, "Syötä kelvollinen kokonaisluku.");
+          return;
+      }
+      pisteet[nykyinenPelaaja] += lisattavatPisteet;
+      if (pisteet[nykyinenPelaaja] > 50) {
+          pisteet[nykyinenPelaaja] = 25;
+      }
+      pelaajanTiedot();
+      if (pisteet[nykyinenPelaaja] == 50) {
+          peliPaattynyt = true;
+          JOptionPane.showMessageDialog(this, pelaajat[nykyinenPelaaja] + " voitti pelin!");
+      } else {
+          nykyinenPelaaja++;
+          if (nykyinenPelaaja == pelaajat.length) {
+              nykyinenPelaaja = 0;
+          }
+          pelaajanTiedot();
+      }
+      pistemaaraKentta.setText("");
+  }
+  
+          public static void main(String[] args) {
+            new PeliOhjelma();
+        }
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            lisaaPisteet();
+        }
+      }        
+
+
